3c94a2ca5f28aef87618e070b1010b4ef8a78a31
Jessica Barnett <BarnettEffect@gmail.com>
2015-02-01 21:17:09 -0500

    Adds page assets, gets footer and header templates working

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,35 +1,8 @@
 this["App"] = this["App"] || {};
 this["App"]["templates"] = this["App"]["templates"] || {};
-this["App"]["templates"]["shoe_list"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "<li class=\"shoes\">\n  <a href=\"/"
-    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "\">"
-    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + " -- Price: "
-    + escapeExpression(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"price","hash":{},"data":data}) : helper)))
-    + " </a>\n</li>\n";
-},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, buffer = "<!--Sample Template-->\n\n\n";
-  stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
-  if (stack1 != null) { buffer += stack1; }
-  return buffer;
-},"useData":true});
-this["App"]["templates"]["user_table"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "    <tr>\n      <td>"
-    + escapeExpression(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"username","hash":{},"data":data}) : helper)))
-    + "</td>\n      <td>"
-    + escapeExpression(((helper = (helper = helpers.firstName || (depth0 != null ? depth0.firstName : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"firstName","hash":{},"data":data}) : helper)))
-    + " "
-    + escapeExpression(((helper = (helper = helpers.lastName || (depth0 != null ? depth0.lastName : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"lastName","hash":{},"data":data}) : helper)))
-    + "</td>\n      <td>"
-    + escapeExpression(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"email","hash":{},"data":data}) : helper)))
-    + "</td>\n    </tr>\n";
-},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, helper, options, functionType="function", helperMissing=helpers.helperMissing, blockHelperMissing=helpers.blockHelperMissing, buffer = "<table>\n  <thead>\n    <th>Username</th>\n    <th>Real Name</th>\n    <th>Email</th>\n  </thead>\n  <tbody>\n";
-  stack1 = ((helper = (helper = helpers.users || (depth0 != null ? depth0.users : depth0)) != null ? helper : helperMissing),(options={"name":"users","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data}),(typeof helper === functionType ? helper.call(depth0, options) : helper));
-  if (!helpers.users) { stack1 = blockHelperMissing.call(depth0, stack1, options); }
-  if (stack1 != null) { buffer += stack1; }
-  return buffer + "  </tbody>\n</table>\n";
-},"useData":true});
+this["App"]["templates"]["footer"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<footer class=\"mainFooter\">\n  <p> This very simple blog was created by Jessica Barnett, with the intent to learn Angular.js </p>\n  <section class=\"socialWrap\">\n    <a href=\"#\"><img class=\"social\" src=\"images/social/facebook.svg\" alt=\"go to facebook\"/></a>\n    <a href=\"#\"><img class=\"social\" src=\"images/social/twitter.svg\" alt=\"go to twitter\"/></a>\n    <a href=\"#\"><img class=\"social\" src=\"images/social/pinterest.svg\" alt=\"go to pinterest\"/></a>\n    <a href=\"#\"><img class=\"social\" src=\"images/social/vimeo.svg\" alt=\"go to vimeo\"/></a>\n  </section>\n</footer>\n";
+  },"useData":true});
+this["App"]["templates"]["header"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<img class=\"mainLogo\" src=\"/images/logo.svg\">\n<section class=\"mainTitleAndNav\">\n	<h1 class=\"mainTitle\">A Simple Blog</h1>\n	<nav class=\"mainNav\">\n		<ul>\n			<li><a href=\"/angularBlog/index.html\">Home</a></li>\n			<li><a href=\"/angularBlog/about.html\">About</a></li>\n			<li><a href=\"/angularBlog/search.html\">Search</a></li>\n		</ul>\n	</nav>\n</section>\n";
+  },"useData":true});
