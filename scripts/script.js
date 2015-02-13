b7a06510501bd2162c70a624b7ed720ca7137628
Jessica <Barnett>
2015-02-13 11:48:28 -0500

    Migrates archive page to handlebars

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,117 @@
+var postDataModel = (function(){
+  var posts,
+      request,
 
-$(document).ready(function(){
+  //returns request for data
+  makeRequest = function(){
+    return $.get('/data/blogData.json', function(response){
+      posts = convertDatesToObjects(response);
+    })
+    .done(function(response){
+      console.log("success: ", response)
+    })
+    .fail(function(error){
+      console.log("failure: ", error);
+    });
+  },
 
-  // Renders 2 sample templates.
+  convertDatesToObjects = function(posts){
+    //converts all dates in the base json, both at the post and comment level from "mm/dd/yyyy" strings to date objects
+    posts.forEach(function(post, index, array){
+          post.date = new Date(post.date);
+          post.comments.forEach(function(comment, index, array){
+            comment.date = new Date(comment.date);
+          });
+         });
+         console.log(posts);
+    return posts;
+  },
 
-  function renderTemplates() {
-    $("#page-header").html(App.templates.header([]));
-    $("#page-footer").html(App.templates.footer([]));
+  getPromise = function(){
+    return request.promise();
+  },
 
+  debug = function(){
+    console.log(this);
+    console.log("posts:  ", posts);
+    console.log("request:   ", request);
+  },
+
+  //returns a promise that you can hook callbacks on
+  //to do stuff after data loads.
+  //data will be available
+  init = function() {
+    request = makeRequest();
+    return this; //chainable
+  };
+
+  return {
+    init : init,
+    getPromise : getPromise,
+    debug : debug
   }
+})();
 
-  renderTemplates();
+
+
+/*Possible helpers:
+
+//angular filters:
+// | limitTo(200);
+// | date
+// possibly helpers for sorting, when I get to the archive page
+
+//custom date fiter
+blogApp.filter('date', function($filter){
+	return function(dateObject){
+		return (dateObject.getMonth()+1)+"/"+dateObject.getDate()+"/"+dateObject.getFullYear();
+	};
 });
+
+//filter to convert post titles into url strings
+blogApp.filter('toUrl', function($filter){
+	return function(postTitle){
+		postTitle = postTitle.replace(/ /g, "_").replace(/[\.,-\/#!$%\^&\*;:{}=\-`~()']/g, "");
+		return postTitle.toLowerCase();
+	};
+});
+
+*/
+//custom date fiter
+var formatDate = function(dateObject){
+	return (dateObject.getMonth()+1)+"/"+dateObject.getDate()+"/"+dateObject.getFullYear();
+};
+
+var limitTo200 = function(text) {
+  return text.slice(0, 200);
+}
+
+//curries and returns a handlebars helper
+var getMonthName = function() {
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return function(monthNumber){
+    return months[monthNumber];
+  }
+}();
+
+Handlebars.registerHelper('getMonthName', getMonthName);
+Handlebars.registerHelper('limitTo200', limitTo200);
+Handlebars.registerHelper('formatDate', formatDate);
+
+function renderTemplates(postsPromise) {
+
+  $("#page-header").html(App.templates.header({}));
+  $("#page-footer").html(App.templates.footer({}));
+
+  postsPromise.done(function(postsData){
+    $("#archive-list").html(App.templates.blog_archive({'posts': postsData}));
+  });
+
+
+}
+
+//starts ajax call
+postDataModel.init();
+
+//document.ready not working.  had to move script to bottom.  : (
+$(document).ready(renderTemplates(postDataModel.getPromise()));
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,8 +1,68 @@
 this["App"] = this["App"] || {};
 this["App"]["templates"] = this["App"]["templates"] || {};
+this["App"]["templates"]["blog_archive"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+  var stack1, helper, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function", lambda=this.lambda, buffer = "\n	<section class=\"post-preview\">\n		<section class=\"post-details\">\n			<time datetime=\""
+    + escapeExpression(((helpers.formatDate || (depth0 && depth0.formatDate) || helperMissing).call(depth0, (depth0 != null ? depth0.date : depth0), {"name":"formatDate","hash":{},"data":data})))
+    + "\"> "
+    + escapeExpression(((helpers.formatDate || (depth0 && depth0.formatDate) || helperMissing).call(depth0, (depth0 != null ? depth0.date : depth0), {"name":"formatDate","hash":{},"data":data})))
+    + "</time>\n			<ul class=\"post-tags\">\n";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.tags : depth0), {"name":"each","hash":{},"fn":this.program(2, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "			</ul>\n			<a href=\"blog_pages/"
+    + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
+    + "\"> "
+    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.comments : depth0)) != null ? stack1.length : stack1), depth0))
+    + " comments </a>\n		</section>\n\n		<section class=\"post-content\">\n			<h2 class=\"post-title\">"
+    + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
+    + "</h2>\n			<p class=\"byline\">"
+    + escapeExpression(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"author","hash":{},"data":data}) : helper)))
+    + "</p>\n			<p>\""
+    + escapeExpression(((helpers.limitTo200 || (depth0 && depth0.limitTo200) || helperMissing).call(depth0, (depth0 != null ? depth0.body : depth0), {"name":"limitTo200","hash":{},"data":data})))
+    + "...\"</p>\n			<a href=\"blog_pages/"
+    + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
+    + "\">more</a>\n		</section>\n	</section>\n\n";
+},"2":function(depth0,helpers,partials,data) {
+  var lambda=this.lambda, escapeExpression=this.escapeExpression;
+  return "					<li><a href=\"#\">"
+    + escapeExpression(lambda(depth0, depth0))
+    + ", </a></li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.posts : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer;
+},"useData":true});
+this["App"]["templates"]["blog_post"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+  var stack1, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda, buffer = "<section class=\"post-details\">\n	<time datetime=\""
+    + escapeExpression(((helpers.formatDate || (depth0 && depth0.formatDate) || helperMissing).call(depth0, (depth0 != null ? depth0.date : depth0), {"name":"formatDate","hash":{},"data":data})))
+    + "\"> "
+    + escapeExpression(((helpers.formatDate || (depth0 && depth0.formatDate) || helperMissing).call(depth0, (depth0 != null ? depth0.date : depth0), {"name":"formatDate","hash":{},"data":data})))
+    + "</time>\n\n	<ul class=\"post-tags\">\n";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.tags : depth0), {"name":"each","hash":{},"fn":this.program(2, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "	</ul>\n\n	<a href=\"#\"> "
+    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.post : depth0)) != null ? stack1.comments : stack1)) != null ? stack1.length : stack1), depth0))
+    + " comments </a>\n</section>\n\n<section class=\"post-content\">\n\n	<h2 class=\"post-title\">"
+    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.post : depth0)) != null ? stack1.title : stack1), depth0))
+    + "</h2>\n	<p class=\"byline\">"
+    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.post : depth0)) != null ? stack1.author : stack1), depth0))
+    + "</p>\n	<p>"
+    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.post : depth0)) != null ? stack1.body : stack1), depth0))
+    + "</p>\n\n</section>\n\n<section class=\"post-comments\">\n	Comments will go here\n</section>\n\n";
+},"2":function(depth0,helpers,partials,data) {
+  var lambda=this.lambda, escapeExpression=this.escapeExpression;
+  return "			<li><a href=\"#\">"
+    + escapeExpression(lambda(depth0, depth0))
+    + ", </a></li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.posts : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer;
+},"useData":true});
 this["App"]["templates"]["footer"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<footer class=\"mainFooter\">\n  <p> This very simple blog was created by Jessica Barnett, with the intent to learn Angular.js </p>\n  <section class=\"socialWrap\">\n    <a href=\"#\"><img class=\"social\" src=\"images/social/facebook.svg\" alt=\"go to facebook\"/></a>\n    <a href=\"#\"><img class=\"social\" src=\"images/social/twitter.svg\" alt=\"go to twitter\"/></a>\n    <a href=\"#\"><img class=\"social\" src=\"images/social/pinterest.svg\" alt=\"go to pinterest\"/></a>\n    <a href=\"#\"><img class=\"social\" src=\"images/social/vimeo.svg\" alt=\"go to vimeo\"/></a>\n  </section>\n</footer>\n";
+  return "<p> This very simple blog was created by Jessica Barnett, with the intent to learn JQuery Promises, Gulp, Handlebars, Express, and node. </p>\n<section class=\"social-wrap\">\n  <a href=\"#\"><img class=\"social\" src=\"images/social/facebook.svg\" alt=\"go to facebook\"/></a>\n  <a href=\"#\"><img class=\"social\" src=\"images/social/twitter.svg\" alt=\"go to twitter\"/></a>\n  <a href=\"#\"><img class=\"social\" src=\"images/social/pinterest.svg\" alt=\"go to pinterest\"/></a>\n  <a href=\"#\"><img class=\"social\" src=\"images/social/vimeo.svg\" alt=\"go to vimeo\"/></a>\n</section>\n";
   },"useData":true});
 this["App"]["templates"]["header"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<img class=\"mainLogo\" src=\"/images/logo.svg\">\n<section class=\"mainTitleAndNav\">\n	<h1 class=\"mainTitle\">A Simple Blog</h1>\n	<nav class=\"mainNav\">\n		<ul>\n			<li><a href=\"/angularBlog/index.html\">Home</a></li>\n			<li><a href=\"/angularBlog/about.html\">About</a></li>\n			<li><a href=\"/angularBlog/search.html\">Search</a></li>\n		</ul>\n	</nav>\n</section>\n";
+  return "<img class=\"main-logo\" src=\"/images/logo.svg\">\n<section class=\"main-title-and-nav\">\n	<h1 class=\"main-title\">A Simple Blog</h1>\n	<nav class=\"main-nav\">\n		<ul>\n			<li><a href=\"/angularBlog/index.html\">Home</a></li>\n			<li><a href=\"/angularBlog/about.html\">About</a></li>\n			<li><a href=\"/angularBlog/search.html\">Search</a></li>\n		</ul>\n	</nav>\n</section>\n";
   },"useData":true});
